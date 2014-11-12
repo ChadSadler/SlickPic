a981609171712b6ad889692f0507d8276ca9b30a
ChadSadler <csad7639@students.vsu.edu>
2014-11-11 20:36:21 -0500

    Button functionality and finishing touches

--- SlickPic/src/com/example/slickpic/PictureView.java
+++ SlickPic/src/com/example/slickpic/PictureView.java
@@ -4,11 +4,14 @@ import java.io.File;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 public class PictureView extends Activity {
@@ -22,25 +25,33 @@ public class PictureView extends Activity {
 			
 		File imgFile = new File(fileUri);
 		String path = imgFile.getAbsolutePath();
-		Uri pathUri = Uri.parse(path);
-
-		ImageView myImage = (ImageView) findViewById(R.id.imgView);
-
-		myImage.setImageURI(pathUri);
-		myImage.setRotation(90);
 		
+		Bitmap source = BitmapFactory.decodeFile(path);
+		
+		ImageView myImage = (ImageView) findViewById(R.id.imgView);
+		
+		Matrix matrix = new Matrix();
+		matrix.postRotate(90);
+		Bitmap image = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+
+		myImage.setImageBitmap(image);
+
 		ImageButton noButton = (ImageButton) findViewById(R.id.noButton);
 		ImageButton yesButton = (ImageButton) findViewById(R.id.yesButton);
 		
+		final Intent mainActivityIntent = new Intent(PictureView.this, MainActivity.class);
+		
 		noButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
-
+				startActivity(mainActivityIntent);
 			}
 		});
 		
 		yesButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
-
+				Toast.makeText(PictureView.this, "Image Sent", Toast.LENGTH_LONG).show();
+				
+				startActivity(mainActivityIntent);
 			}
 		});
 		}
