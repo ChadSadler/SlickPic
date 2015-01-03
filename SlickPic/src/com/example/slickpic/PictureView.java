a8b4998818ff28a7c4abf342df387e14afe7da1d
ChadSadler <csad7639@students.vsu.edu>
2015-01-02 19:53:26 -0500

    Revert "Button functionality and finishing touches"
    This reverts commit a981609171712b6ad889692f0507d8276ca9b30a.

--- SlickPic/src/com/example/slickpic/PictureView.java
+++ SlickPic/src/com/example/slickpic/PictureView.java
@@ -4,14 +4,11 @@ import java.io.File;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 public class PictureView extends Activity {
@@ -25,33 +22,25 @@ public class PictureView extends Activity {
 			
 		File imgFile = new File(fileUri);
 		String path = imgFile.getAbsolutePath();
-		
-		Bitmap source = BitmapFactory.decodeFile(path);
-		
+		Uri pathUri = Uri.parse(path);
+
 		ImageView myImage = (ImageView) findViewById(R.id.imgView);
+
+		myImage.setImageURI(pathUri);
+		myImage.setRotation(90);
 		
-		Matrix matrix = new Matrix();
-		matrix.postRotate(90);
-		Bitmap image = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
-
-		myImage.setImageBitmap(image);
-
 		ImageButton noButton = (ImageButton) findViewById(R.id.noButton);
 		ImageButton yesButton = (ImageButton) findViewById(R.id.yesButton);
 		
-		final Intent mainActivityIntent = new Intent(PictureView.this, MainActivity.class);
-		
 		noButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
-				startActivity(mainActivityIntent);
+
 			}
 		});
 		
 		yesButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
-				Toast.makeText(PictureView.this, "Image Sent", Toast.LENGTH_LONG).show();
-				
-				startActivity(mainActivityIntent);
+
 			}
 		});
 		}
